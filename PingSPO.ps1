da94db7103ffc77eacbfe13beb469f5fcbc08cca
Kengo Mori <kenmori@microsoft.com>
2019-07-20 09:18:48 +0900

    Initial Commit

---
+++ PingSPO.ps1
@@ -0,0 +1,78 @@
+﻿
+param(
+  [Parameter(Mandatory=$true)]
+  $SiteUrl,
+  $AlertMe = $false,
+  $Number = 4,
+  $ResetCred = $false,
+  $IntervalSecond = 1
+)
+
+
+### Alert Settings ###
+$durationAlert = 30000 # 30 seconds (30 sec x 1000 millisec)
+$healthAlert = 9
+$statusAlert = 400
+###
+
+if (($global:PingCred -eq $null) -or $ResetCred)
+{
+  $global:PingCred = Get-Credential
+}
+
+
+function GetHostAddress($strUrl)
+{
+  $spaddr = "sharepoint.com"
+  return $strUrl.SubString(0, $strUrl.IndexOf($spaddr) + $spaddr.Length)
+}
+
+function SendAlert($ToAddress, $Subject, $Body)
+{
+
+  $emailProperties = New-Object Microsoft.SharePoint.Client.Utilities.EmailProperties
+  $emailProperties.To = [String[]]($ToAddress)
+  $emailProperties.Subject = $Subject
+  $emailProperties.Body = $Body
+  [Microsoft.SharePoint.Client.Utilities.Utility]::SendEmail($script:context,$emailProperties)
+
+  $script:context.ExecuteQuery()
+}
+
+[void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client")
+[void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Client.Runtime")
+
+$script:context = New-Object Microsoft.SharePoint.Client.ClientContext($SiteUrl)
+
+$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($global:PingCred.UserName, $global:PingCred.Password) 
+$script:context.Credentials = $credentials
+
+$script:mySession = New-Object -TypeName Microsoft.PowerShell.Commands.WebRequestSession
+$myCookie = New-Object -TypeName System.Net.Cookie("SPOIDCRL", $credentials.GetAuthenticationCookie($SiteUrl).Replace("SPOIDCRL=", ""))
+$script:mySession.Cookies.Add((GetHostAddress -strUrl $SiteUrl), $myCookie)
+
+for ($i = 0; $i -lt $Number; $i++)
+{
+  $d = Get-Date
+  $data = Invoke-WebRequest -Uri $SiteUrl -WebSession $mySession -MaximumRedirection 0
+  [int]$duration = ((Get-Date) - $d).TotalMilliseconds
+  [int]$statuscode = $data.StatusCode
+  [int]$healthscore = $data.Headers["x-sharepointhealthscore"]
+  [string]$correlationid = $data.Headers["sprequestguid"]
+  $out = ("StatusCode: " + $statuscode + "; Duration (msec): " + $duration + "; SharePointHealthScore: " + $healthscore)
+
+  if (($statuscode -ge $statusAlert) -or ($duration -gt $durationAlert) -or ($healthscore -ge $healthAlert))
+  {
+    $out += ("; CorrelationId: " + $correlationid)
+    if ($AlertMe)
+    {
+      $subject = ("PingSPO Alert " + ((Get-Date).ToString((Get-Culture).DateTimeFormat.ShortDatePattern + " hh:mm:ss")))
+      $body = ("<B>Alert Detail</B><BR>" + $out.Replace("; ", "<BR>"))
+      SendAlert -ToAddress $global:PingCred.UserName -Subject $subject -Body $body
+    }
+  }
+  $out  
+
+  Start-Sleep -Second $IntervalSecond
+}
+
---
+++ Readme.md
@@ -0,0 +1,39 @@
+# PingSPO
+
+This is a Ping PowerShell Script to acquire the health status of the SharePoint Online. 
+HTTP Status Code, Duration (msec), SharePointHealthScore will be displayed.
+And if there is any alert, you will see the Correlation ID as well.
+With additional option (-AlertMe $true), you will be able to receive e-mail.
+
+## Prerequisite
+
+You need to download SharePoint Online Client Components SDK to run this script. 
+
+https://www.microsoft.com/en-us/download/details.aspx?id=42038
+
+
+## How To Run This Command
+
+-SiteUrl ... The SiteURL to ping.
+
+-AlertMe ... [optional] $true if you want to receive alert e-mail (Default : $false)
+
+-Number ... [optional] Number of Ping Request (Default : 4)
+
+-ResetCred ... The credential will be stored on this script. If you want to reset the credential, please set this to $true (Default : $false)
+
+-IntervalSecond ... [optional] The interval second of the ping request. Suggest you to set longer value, if you want to make large number of trials to ping by -Number option.
+
+## Example 1
+
+.\PingSPO.ps1 -SiteUrl https://tenant.sharepoint.com
+
+## Example 2
+
+.\PingSPO.ps1 -SiteUrl https://tenant.sharepoint.com -AlertMe $true -Number 320 -IntervalSecond (60 * 15)
+
+## Reference
+Please also check the following Docs Script for the best practices.
+
+https://docs.microsoft.com/en-us/office365/enterprise/diagnosing-performance-issues-with-sharepoint-online
+
